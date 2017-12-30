ba55660e90fbb20116bf126e66e267efd04d61c3
Hancel.Lin <imlinhanchao@gmail.com>
2017-12-30 17:05:54 +0800

    新增同步价格Node.js脚本。

---
+++ sync.js
@@ -0,0 +1,31 @@
+let House = require("./app/house");
+
+(async () => {
+    let infos = [];
+    let failed = {
+        page: [],
+        id: []
+    }
+    for (let j = 0; j < 100; j++) {
+        try {
+            console.log(`Get page ${j + 1} info ing...`);                        
+            let infos = await House.getSummarys(j + 1);
+            for (let i = 0; i < infos.length; i++) {
+                try {
+                    await House.updatePrice(infos[i]);
+                    console.log(`Update id ${infos[i].id} price success!`);
+                } catch (error) {
+                    console.log(`Update id ${infos[i].id} price failed!`);
+                    console.log(`error message:${error}`);
+                    failed.id.push(infos[i].id);
+                }
+            }
+            console.log(`Get page ${j + 1} info success!`);            
+        } catch (error) {
+            console.log(`Get page ${j + 1} info failed!`);            
+            console.log(`error message:${error}`);
+            failed.id.push(j + 1);            
+        }
+    }
+    console.log(JSON.stringify(failed));
+})();
